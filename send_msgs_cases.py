fdaa49e2f88d735f6e243cb296abf7fbe7bcb330
princelySid <sidney.ochieng@gmail.com>
2018-11-16 14:34:00 +0300

    Send messages from cases

---
+++ send_msgs_cases.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+HEADER = {'Authorization': 'Token <token>'}
+
+
+sms1 = input.get('sms1')
+sms2 = input.get('sms2')
+sms3 = input.get('sms3')
+
+uuids = input.get('uuids')
+flow_id = '<flow_id>'
+case_id = input.get('case_id')
+msg_ids = input.get('message_ids')
+
+contact_url = 'https://app.rapidpro.io/api/v2/contacts.json'
+flow_url = 'https://app.rapidpro.io/api/v2/flow_starts.json'
+
+uuids = uuids.split(',')
+list_urns = []
+for uuid in uuids:
+    DATA =  {'uuid' : uuid}
+
+    response = requests.get(contact_url, headers=HEADER, params=DATA, verify=True)
+    list_urns.append(str(response.json()['results'][0]['urns'][0]))
+
+
+extra = {"case_id":case_id, "sms1":sms1, "sms2":sms2, "sms3":sms3, "msg_ids":msg_ids}
+extra = json.dumps(extra)
+
+DATA =  {"flow": flow_id,
+		 "groups": [],
+		 "contacts": [],
+		 "urns": list_urns,
+         "restart_participants":"True",
+		 "extra": extra
+		 }
+response = requests.post(flow_url, headers=HEADER, verify=True, data=DATA)
